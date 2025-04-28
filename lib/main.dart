53213aa29b90fe75f9084c643ea4aa676ef72166
kantaP <acromod@gmail.com>
2025-04-28 09:53:26 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zimpligital_assignment/presentation/pages/music_player_screen/index.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
